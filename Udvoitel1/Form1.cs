5c04e1aa50c3f5c8666d554e4d375621409c61d8
Likeside <canuckvisa@gmail.com>
2020-05-26 11:14:05 +0300

    Homework7 added

---
+++ Udvoitel1/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Udvoitel.Model;
+
+//Зиновьев А.А.
+/*1. а) Добавить в программу «Удвоитель» подсчет количества отданных команд.
+б) Добавить меню и команду «Играть». При нажатии появляется сообщение, какое число должен получить игрок. Игрок должен постараться получить это число за минимальное количество ходов.
+в) * Добавить кнопку «Отменить», которая отменяет последние ходы.
+*/
+namespace Udvoitel
+{
+    public partial class Form1 : Form
+    {
+        GameDoubler game;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show(DateTime.Now.ToLongTimeString());
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure?","Warning",MessageBoxButtons.YesNo)==DialogResult.Yes)  this.Close();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            game = new GameDoubler();
+            game.action += UpdateInfo;
+            UpdateInfo();
+            btnMulti.Enabled = true;
+            btnPlus.Enabled = true;
+            button1.Enabled = true;
+            button2.Enabled = true;
+            
+        }
+
+        void UpdateInfo()
+        {
+            lblCount.Text = "Ходов сделано: " + game.Count.ToString();
+            lblCurrent.Text = "Ваше число: " + game.Current.ToString();
+            lblFinish.Text = "Вам нужно получить число " + game.Finish.ToString();
+            lblTotalMoves.Text = "Всего действий предпринято: " + game.TotalMoves.ToString();
+            lblMinSteps.Text = "Минимальное количество хоодов: " + game.Steps.ToString();
+
+            if (game.Current == game.Finish && game.Count == game.Steps)
+            {
+                lblWinLose.Text = "Вы победили";
+            }
+            else if (game.Count > game.Steps)
+            {
+                lblWinLose.Text = "Вы проиграли";
+            }
+            else
+            {
+                lblWinLose.Text = "Вы пока не победили и не проиграли";
+            }
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            game.Plus();
+            
+        }
+
+        private void btnMulti_Click(object sender, EventArgs e)
+        {
+            game.Multi();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            game.Back();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            game.Reset();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Exchange.Current = game.Current;
+            formAbout about = new formAbout();
+            about.ShowDialog();
+        }
+
+      
+    }
+}
---
+++ Udvoitel1/Model/GameDoubler.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udvoitel.Model
+{
+    enum StatusGame
+    {
+        Win, Lose, Play
+    }
+
+
+    delegate void Action();
+
+    class GameDoubler
+    {
+        int current;
+        int totalMoves;
+        public Action action;
+
+        public int Finish { get; private set; }
+        Stack<int> history = new Stack<int>();
+
+
+        /// <summary>
+        /// Текущее число
+        /// </summary>
+        public int Current
+        {
+            get { return current; }
+        }
+
+        public int TotalMoves
+        {
+            get { return totalMoves; }
+        }
+
+        /// <summary>
+        /// Кол-во ходов
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                return history.Count;
+            }
+        }
+            
+
+        public int Steps//минимальное кол-во шагов
+        {
+            get
+            {
+                int f = Finish;//50
+                int i = 0;
+                while (f != 1)
+                {
+                    if (f % 2 == 0) f = f / 2;
+                      else f--;
+                    //f = f % 2 == 0 ? f / 2 : f - 1;
+                    i++;
+                }
+                return i;
+            }
+        }
+
+        public GameDoubler(int min, int max)
+        {
+            Finish = new Random().Next(min, max + 1);
+            current = 1;
+            totalMoves = 0;
+        }
+
+        public GameDoubler()
+        {
+            Finish = new Random().Next(10, 101);
+            current = 1;
+            totalMoves = 0;
+        }
+
+        public GameDoubler(int finish)
+        {
+            this.Finish = finish;
+            current = 1;
+            totalMoves = 0;
+        }
+
+
+        public int Plus()
+        {
+            history.Push(current);
+            current++;
+            totalMoves++;
+            action();
+            return current;
+        }
+
+        public int Multi()
+        {
+            history.Push(current);
+            current *= 2;
+            totalMoves++;
+            action();
+            return current;
+        }
+
+        public void Reset()
+        {
+            current = 1;
+            totalMoves++;
+            action();
+            history.Clear();            
+        }
+
+        public int Back()
+        {
+            totalMoves++;
+            action();
+            //if (history.Count != 0) return history.Pop(); else return 1;
+
+            if (history.Count != 0)
+            {
+                current = history.Pop();
+                return current;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        public override string ToString()
+        {
+            return current.ToString();
+        }
+
+        
+    }
+}
